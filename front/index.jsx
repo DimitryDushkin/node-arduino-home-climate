c8f17025a3560258952de65bd69ccd96ca16952b
Dmitry Dushkin <dndushkin@yandex-team.ru>
2016-05-22 21:44:20 +0300

    Switch to material ui

--- front/index.jsx
+++ front/index.jsx
@@ -1,6 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
-import Content from './container/Content.jsx';
+import RaisedButton from 'material-ui/RaisedButton';
+import AppBar from 'material-ui/AppBar';
+import Paper from 'material-ui/Paper';
 
-ReactDOM.render(<Content />, document.querySelector('.app-container'));
+import {lightBlue500, lightBlue700} from 'material-ui/styles/colors'
+
+import './index.css';
+
+const theme = getMuiTheme({
+    palette: {
+        primary1Color: lightBlue500,
+        primary2Color: lightBlue700
+    }
+});
+
+const App = () => (
+<MuiThemeProvider muiTheme={theme}>
+    <div>
+        <AppBar
+            showMenuIconButton={false}
+            title={"Дом"} />
+        <div className="App__content">
+            <Paper className="App__section">
+                <h3>Присутствие дома</h3>
+            </Paper>
+        </div>
+    </div>
+</MuiThemeProvider>
+);
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
